41d210668f0a98d68f8c06c5d2028ce17b7bfb43
Ital023 <contadoitalo911@gmail.com>
2024-08-03 19:05:33 -0300

    feat: URI 2621 finished SQL and JPQL

--- uri2621/src/main/java/com/devsuperior/uri2621/Uri2621Application.java
+++ uri2621/src/main/java/com/devsuperior/uri2621/Uri2621Application.java
@@ -1,11 +1,16 @@
 package com.devsuperior.uri2621;
 
+import com.devsuperior.uri2621.dto.ProductMinDTO;
+import com.devsuperior.uri2621.projections.ProductMinProjection;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import com.devsuperior.uri2621.repositories.ProductRepository;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @SpringBootApplication
 public class Uri2621Application implements CommandLineRunner {
@@ -19,6 +24,21 @@ public class Uri2621Application implements CommandLineRunner {
 
 	@Override
 	public void run(String... args) throws Exception {
-		
+
+		/*
+		List<ProductMinProjection> list = repository.search1(10,20,"P");
+		List<ProductMinDTO> objList = list.stream().map(x -> new ProductMinDTO(x)).collect(Collectors.toList());
+
+		for(ProductMinDTO obj : objList){
+			System.out.println(obj);
+		}*/
+
+		List<ProductMinDTO> objList = repository.search2(10,20,"P");
+
+		for(ProductMinDTO obj : objList){
+			System.out.println(obj);
+		}
+
+
 	}
 }
